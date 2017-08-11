8f86fa9c2bca79a0c3942baa01b90d3f84f4b325
echoguan <634561159@qq.com>
2017-08-12 00:22:56 +0800

    html and js

--- src/main/resources/templates/js/compare.js
+++ src/main/resources/templates/js/compare.js
@@ -1,1 +1,14 @@
-111
+$(document).ready(function(){
+    $("#compareColumnBetweenSameTable").click(function(){
+        $.post("compare/compareColumnBetweenSameTableName", {
+            "ip": $("#ip").val(),
+            "port": $("#port").val(),
+            "userName": $("#userName").val(),
+            "password": $("#password").val(),
+            "db1Name": $("#db1Name").val(),
+            "db2Name": $("#db2Name").val()
+        },function(data,status){
+            alert("数据：" + data + "\n状态：" + status);
+        });
+    });
+});
