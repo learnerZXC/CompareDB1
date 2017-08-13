68159ae76424610a80b4e064044efcea5ae363b4
echoguan <634561159@qq.com>
2017-08-14 01:22:09 +0800

    complete all functions expect "一键对比"

--- src/main/resources/templates/js/compare.js
+++ src/main/resources/templates/js/compare.js
@@ -1,25 +1,4 @@
 $(document).ready(function(){
-    $("#compareColumnBetweenSameTableName").click(function(){
-        $.post("compare/compareColumnBetweenSameTableName", {
-            "ip": $("#ip").val(),
-            "port": $("#port").val(),
-            "userName": $("#userName").val(),
-            "password": $("#password").val(),
-            "db1Name": $("#db1Name").val(),
-            "db2Name": $("#db2Name").val()
-        },function(data,status){
-            if(status === 'success'){
-                alert("success!");
-                // var dbData = data;
-                // var firstLine = "<tr><td>111</td><td>222</td><td >333</td><td>444</td><td>555</td></tr>";
-                // console.log("dbData.length: "+dbData.length);
-                // // $("#resultTable").html(firstLine);
-                // $.each(dbData, function (n, value) {
-                //     console.log(n+"----"+value.db1Name);
-                // })
-            }
-        });
-    });
 
     $("#compareTableBetweenDifferentDB").click(function(){
         $.post("compare/compareTableBetweenDifferentDB", {
@@ -33,14 +12,39 @@ $(document).ready(function(){
             if(status === 'success'){
                 var dbData = data;
                 var table;
-                var trH = "<thead><tr><td>数据库名</td><td>数据表名</td><td >数据库名</td><td>数据表名</td></tr></thead>";
+                var trH = "<thead><tr><td>数据库名</td><td>数据表名</td></tr></thead>";
                 var trB = "<tbody>"
                 $.each(dbData, function (n, value) {
-                    trB += "<tr><td>"+ value.db1Name +"</td><td>"+value.db1TableName+"</td><td >"+value.db2Name+"</td><td>"+value.db2TableName+"</td></tr>";
+                    trB += "<tr><td>"+ value.db1Name +"</td><td>"+value.db1TableName+"</td></tr>";
                 })
                 trB += "</tbody>"
                 table = trH + trB;
-                $("#table-title").text("两数据库互不存在的表为：");
+                $("#table-title").text("两数据库互不存在的表有：");
+                $("#resultTable").html(table);
+            }
+        });
+    });
+
+    $("#compareColumnBetweenSameTableName").click(function(){
+        $.post("compare/compareColumnBetweenSameTableName", {
+            "ip": $("#ip").val(),
+            "port": $("#port").val(),
+            "userName": $("#userName").val(),
+            "password": $("#password").val(),
+            "db1Name": $("#db1Name").val(),
+            "db2Name": $("#db2Name").val()
+        },function(data,status){
+            if(status === 'success'){
+                var dbData = data;
+                var table;
+                var trH = "<thead><tr><td>数据库名</td><td>数据表名</td><td >列名</td></tr></thead>";
+                var trB = "<tbody>"
+                $.each(dbData, function (n, value) {
+                    trB += "<tr><td>"+ value.db1Name +"</td><td>"+value.db1TableName+"</td><td >"+value.db1ColumnName+"</td></tr>";
+                })
+                trB += "</tbody>"
+                table = trH + trB;
+                $("#table-title").text("两数据库中相同表 列名不一致的有：");
                 $("#resultTable").html(table);
             }
         });
@@ -56,18 +60,92 @@ $(document).ready(function(){
             "db2Name": $("#db2Name").val()
         },function(data,status){
             if(status === 'success'){
-                alert("success");
-                // var dbData = data;
-                // var table;
-                // var trH = "<thead><tr><td>数据库名</td><td>数据表名</td><td >数据库名</td><td>数据表名</td></tr></thead>";
-                // var trB = "<tbody>"
-                // $.each(dbData, function (n, value) {
-                //     trB += "<tr><td>"+ value.db1Name +"</td><td>"+value.db1TableName+"</td><td >"+value.db2Name+"</td><td>"+value.db2TableName+"</td></tr>";
-                // })
-                // trB += "</tbody>"
-                // table = trH + trB;
-                // $("#table-title").text("两数据库互不存在的表为：");
-                // $("#resultTable").html(table);
+                var dbData = data;
+                var table;
+                var trH = "<thead><tr><td>数据库名</td><td>数据表名</td><td>列名</td><td>数据类型</td><td >数据库名</td><td>数据表名</td><td>列名</td><td>数据类型</td></tr></thead>";
+                var trB = "<tbody>"
+                $.each(dbData, function (n, value) {
+                    trB += "<tr><td>"+ value.db1Name +"</td><td>"+value.db1TableName+"</td><td >"+value.db1ColumnName+"</td><td>"+value.db1DataType+"</td><td>"+ value.db2Name +"</td><td>"+value.db2TableName+"</td><td >"+value.db2ColumnName+"</td><td>"+value.db2DataType+"</td></tr>";
+                })
+                trB += "</tbody>"
+                table = trH + trB;
+                $("#table-title").text("两数据库中相同表 相同列名 数据类型不一致的有：");
+                $("#resultTable").html(table);
+            }
+        });
+    });
+
+    $("#compareColumnTypeBetweenSameTableName").click(function(){
+        $.post("compare/compareColumnTypeBetweenSameTableName", {
+            "ip": $("#ip").val(),
+            "port": $("#port").val(),
+            "userName": $("#userName").val(),
+            "password": $("#password").val(),
+            "db1Name": $("#db1Name").val(),
+            "db2Name": $("#db2Name").val()
+        },function(data,status){
+            if(status === 'success'){
+                var dbData = data;
+                var table;
+                var trH = "<thead><tr><td>数据库名</td><td>数据表名</td><td>列名</td><td>数据类型</td><td>数据类型长度</td><td >数据库名</td><td>数据表名</td><td>列名</td><td>数据类型</td><td>数据类型长度</td></tr></thead>";
+                var trB = "<tbody>"
+                $.each(dbData, function (n, value) {
+                    trB += "<tr><td>"+ value.db1Name +"</td><td>"+value.db1TableName+"</td><td>"+value.db1ColumnName+"</td><td>"+value.db1DataType+"</td><td>"+value.db1ColunmType+"</td><td>"+ value.db2Name +"</td><td>"+value.db2TableName+"</td><td >"+value.db2ColumnName+"</td><td>"+value.db2DataType+"</td><td>"+value.db2ColunmType+"</td></tr>";
+                })
+                trB += "</tbody>"
+                table = trH + trB;
+                $("#table-title").text("两数据库中相同表 相同列名 数据类型相同 数据类型长度不一致的有：");
+                $("#resultTable").html(table);
+            }
+        });
+    });
+
+    $("#compareColumnIsNullBetweenSameTableName").click(function(){
+        $.post("compare/compareColumnIsNullBetweenSameTableName", {
+            "ip": $("#ip").val(),
+            "port": $("#port").val(),
+            "userName": $("#userName").val(),
+            "password": $("#password").val(),
+            "db1Name": $("#db1Name").val(),
+            "db2Name": $("#db2Name").val()
+        },function(data,status){
+            if(status === 'success'){
+                var dbData = data;
+                var table;
+                var trH = "<thead><tr><td>数据库名</td><td>数据表名</td><td>列名</td><td>是否可为空</td><td >数据库名</td><td>数据表名</td><td>列名</td><td>是否可为空</td></tr></thead>";
+                var trB = "<tbody>"
+                $.each(dbData, function (n, value) {
+                    trB += "<tr><td>"+ value.db1Name +"</td><td>"+value.db1TableName+"</td><td>"+value.db1ColumnName+"</td><td>"+value.db1ColumnIsNull+"</td><td>"+ value.db2Name +"</td><td>"+value.db2TableName+"</td><td >"+value.db2ColumnName+"</td><td>"+value.db2ColumnIsNull+"</td></tr>";
+                })
+                trB += "</tbody>"
+                table = trH + trB;
+                $("#table-title").text("两数据库中相同表 相同列名 数据类型相同 数据类型是否可为空不一致的有：");
+                $("#resultTable").html(table);
+            }
+        });
+    });
+
+    $("#compareColumnDefaultValueBetweenSameTableName").click(function(){
+        $.post("compare/compareColumnDefaultValueBetweenSameTableName", {
+            "ip": $("#ip").val(),
+            "port": $("#port").val(),
+            "userName": $("#userName").val(),
+            "password": $("#password").val(),
+            "db1Name": $("#db1Name").val(),
+            "db2Name": $("#db2Name").val()
+        },function(data,status){
+            if(status === 'success'){
+                var dbData = data;
+                var table;
+                var trH = "<thead><tr><td>数据库名</td><td>数据表名</td><td>列名</td><td>数据类型</td><td>默认值</td><td >数据库名</td><td>数据表名</td><td>列名</td><td>数据类型</td><td>默认值</td></tr></thead>";
+                var trB = "<tbody>"
+                $.each(dbData, function (n, value) {
+                    trB += "<tr><td>"+ value.db1Name +"</td><td>"+value.db1TableName+"</td><td>"+value.db1ColumnName+"</td><td>"+value.db1DataType+"</td><td>"+value.db1ColumnDefault+"</td><td>"+ value.db2Name +"</td><td>"+value.db2TableName+"</td><td >"+value.db2ColumnName+"</td><td>"+value.db2DataType+"</td><td>"+value.db2ColumnDefault+"</td></tr>";
+                })
+                trB += "</tbody>"
+                table = trH + trB;
+                $("#table-title").text("两数据库中相同表 相同列名 数据类型相同 默认值不一致的有：");
+                $("#resultTable").html(table);
             }
         });
     });
